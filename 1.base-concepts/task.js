2517f1d2b0f07e5a906054ed07c3287b3b8d30ae
AlexAnt <grauheim.today@gmail.com>
2024-03-09 12:46:57 +0300

    Commit 4

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,16 @@
-"use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b ** 2 -4 * a * c;
-  if (d > 0){
-	arr.push((-b + Math.sqrt(d))/(2*a))
-	arr.push((-b - Math.sqrt(d))/(2*a))
-  }
-  else if(d===0){
-	arr.push(-b/(2*a))
-  }
-  return arr;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+	if (discriminant === 0) {
+		let discriminantSquare = -b / (2 * a);
+		arr.push(discriminantSquare);
+	}
+	else if (discriminant > 0) {
+		let discriminantSquare1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let discriminantSquare2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(discriminantSquare1, discriminantSquare2);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
