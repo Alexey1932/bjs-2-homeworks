191c44633977872e23b8e432e2c4a8e7e6c235d0
AlexAnt <grauheim.today@gmail.com>
2024-03-13 12:49:16 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = []
+  }
+
+let alex = new Student('Алексей', 'мужской', 31);
+let shannon = new Student('Шэннон', 'женский', 22);
+let vasilisa = new Student('Василиса', 'женский', 19);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.marks && this.marks.length > 0){
+	this.marks.push(...marks)
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.marks || this.marks.length === 0){
+	return 0
+  }
+  return (this.marks.reduce((a,b) => a+b, 0))/this.marks.length
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks
+  delete this.subject
 }
