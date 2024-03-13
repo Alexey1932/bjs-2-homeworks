3e83e42d9c74985f9eb15f73097b1b08974eacc6
AlexAnt <grauheim.today@gmail.com>
2024-03-13 13:02:38 +0300

    Commit 3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -15,13 +15,13 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-	if (!(this.excluded)) {
+	if (!(this.excluded) ?? this.marks.length > 0) {
 		this.marks.push(...marks);
 	}
 }
 
 Student.prototype.getAverage = function () {
-  if(!this.marks || this.marks.length === 0){
+  if(this.excluded || this.marks.length === 0){
 	return 0
   }
   return (this.marks.reduce((a,b) => a+b, 0))/this.marks.length;
